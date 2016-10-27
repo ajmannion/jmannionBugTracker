248cee6a285fec25d30c81ebcc3defa5dea854ca
AJ Mannion <john.aj.mannion@gmail.com>
2016-10-27 16:00:27 -0400

    3 on 27 oct 2016

--- jmannionBugTracker/Models/AccountViewModels.cs
+++ jmannionBugTracker/Models/AccountViewModels.cs
@@ -64,6 +64,18 @@ namespace jmannionBugTracker.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
