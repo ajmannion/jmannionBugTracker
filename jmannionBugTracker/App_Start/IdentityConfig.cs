48b918ea2b3970f447e2a2ec931e537850d15be3
AJ Mannion <john.aj.mannion@gmail.com>
2016-11-01 16:25:30 -0400

    adding ticket get create

--- jmannionBugTracker/App_Start/IdentityConfig.cs
+++ jmannionBugTracker/App_Start/IdentityConfig.cs
@@ -11,18 +11,40 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using jmannionBugTracker.Models;
+using SendGrid;
+using System.Net.Mail;
+using System.Configuration;
 
 namespace jmannionBugTracker
 {
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+      
+        public async Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            var apiKey = ConfigurationManager.AppSettings["SendGridAPIKey"];
+            var from = ConfigurationManager.AppSettings["ContactEmail"];
+            SendGridMessage myMessage = new SendGridMessage();
+            myMessage.AddTo(from);
+            myMessage.From = new MailAddress(from);
+            myMessage.Subject = message.Subject;
+            myMessage.Html = message.Body;
+            //create a web transport for sending email
+            var transportWeb = new Web(apiKey);
+            //send the email.
+            try
+            {
+                await transportWeb.DeliverAsync(myMessage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                await Task.FromResult(0);
+            }
         }
     }
-
+    
     public class SmsService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
